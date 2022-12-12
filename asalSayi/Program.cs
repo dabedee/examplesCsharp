bf55faf2b3fbded62c4c7a6bc9398e1e5eff109c
nurul <nurul@DESKTOP-1GAOVS9>
2022-12-12 04:19:31 +0300

    Add project files.

---
+++ asalSayi/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asalSayi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, sayi, sayac = 0;
+            Console.Write("Sayi giriniz : ");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= sayi; i++)
+            {
+                if (sayi % i == 0)
+                {
+                    sayac++;
+
+                }
+            }
+            if (sayac == 2)
+            {
+                Console.WriteLine("{0} sayisi asal sayidir ",sayi);
+            }
+            else
+            {
+                Console.WriteLine("{0} sayisi asal sayi değildir ",sayi);
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ mukemmelSayi/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mukemmelSayi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, sayi, toplam = 0;
+            Console.Write("Sayi giriniz : ");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i < sayi; i++)
+            {
+                if (sayi % i == 0)
+                {
+                    toplam = toplam + i;
+                }
+            }
+            if (toplam == sayi)
+            {
+                Console.WriteLine("Girilen sayi bir Mükemmel Sayidir. ");
+            }
+            else
+            {
+                Console.WriteLine("Girilen sayi mükemmel sayi değildir. ");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
