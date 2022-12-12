e020ff58f8b6b23236e3fdef27d07b63707d21d7
dabedee <83783289+dabedee@users.noreply.github.com>
2022-12-12 05:20:11 +0300

    b54353453545454353

---
+++ factorialOfANumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factorialOfANumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi, i, fak = 1;
+            Console.Write("Sayi giriniz : ");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            for (i = sayi; i > 1; i--)
+            {
+                fak = fak * i;
+            }
+            Console.WriteLine("{0} sayisinin faktoriyelii = {1}",sayi,fak);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ fibonacciSeries/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacciSeries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, c;
+            int a = 0;
+            int b = 1;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            for (i = 0; i < 10; i++)
+            {
+                c = a + b;
+                Console.WriteLine(c);
+                a = b;
+                b = c;
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
