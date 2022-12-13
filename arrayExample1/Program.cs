498eef9285f8ac4ef835047288a2451c36da5b4f
dabedee <83783289+dabedee@users.noreply.github.com>
2022-12-14 01:53:02 +0300

    arra

---
+++ arrayExample1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayExample1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //BİR DİZİDEKİ ANAHTAR ÖĞELERİ ARAMA
+            int n, n1, i, flag = 0;
+            int[] myArr = new int[55];
+            Console.WriteLine("Dizinin boyutunu giriniz = ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dizinin elemanlarını giriniz = ");
+            for (i = 0; i < n; i++)
+            {
+                myArr[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Aranacak elemanı giriniz = ");
+            n1 = int.Parse(Console.ReadLine());
+            for (i = 0; i < n; i++)
+            {
+                if (myArr[i]==n1)
+                {
+                    flag = 1;
+                    Console.WriteLine("Sayı "+n1+ "konumu"+(i+1));
+                    break;
+                }
+            }
+            if (flag == 0)
+            {
+                Console.WriteLine("sayı dizide bulunamadı"+n1);
+                
+            }
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
