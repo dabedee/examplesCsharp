ea795cd3122c36abc9eb805828683c003de6dc04
dabedee <83783289+dabedee@users.noreply.github.com>
2022-12-14 04:19:31 +0300

    b

--- arrayExample2/Program.cs
+++ arrayExample2/Program.cs
@@ -10,6 +10,27 @@ namespace arrayExample2
     {
         static void Main(string[] args)
         {
+            // kullanıcıdan 10 elemanlı BİR DİZİ alan ve  ÇİFT SAYILARIN SAYISINI BULan
+            int i, cnt = 0;
+            int[] myArr = new int[10];
+            Console.WriteLine("sayi giriniz = ");
+            for (i = 0; i < 10; i++)
+            {
+                myArr[i] = int.Parse(Console.ReadLine());
+                if (myArr[i] % 2 == 0)
+                {
+                    cnt = cnt + 1;
+                }
+            }
+            Console.WriteLine("{0} tane çift sayı vardır",cnt);
+            Console.ReadLine();
+
+
+
+
+
+
+
 
         }
     }
---
+++ arrayExample3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayExample3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*dizinin boyutunu ve elemanlarını kullanıcıdan alan
+             * daha sonra dizinin minimum ve ikinci minimum elemanını bulan program
+             */
+            int min1, min2, n, i;
+            int[] myArr = new int[55];
+            Console.WriteLine("Dizinin lenght giriniz = ");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0} adet eleman giriniz ",n);
+            for (i = 0; i < n; i++)
+            {
+                myArr[i] = int.Parse(Console.ReadLine());
+            }
+            if (myArr[0] < myArr[1])
+            {
+                min1= myArr[0];
+                min2= myArr[1];
+            }
+            else
+            {
+                min1 = myArr[1];
+                min2 = myArr[0];
+            }
+            for (i = 2; i < n; i++)
+            {
+                if (myArr[i]<min1)
+                {
+                    min2 = min1;
+                    min1= myArr[i];
+                }
+                else if (myArr[i]<min2)
+                {
+                    min2 = myArr[i];
+                }
+            }
+            Console.WriteLine("min = "+min1);
+            Console.WriteLine("second min = "+min2);
+            Console.ReadLine();
+        }
+    }
+}
