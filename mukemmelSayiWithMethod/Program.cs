e32d1e7c20dffac1708c21e02c1fd106ddfc9d2d
dabedee <83783289+dabedee@users.noreply.github.com>
2022-12-16 05:45:49 +0300

    y

---
+++ mukemmelSayiWithMethod/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mukemmelSayiWithMethod
+{
+    internal class Program
+    {
+        static bool MukemmelSayıMı(int sayi)
+        {
+            int i, toplam = 0;
+            for (i = 1; i < sayi; i++)
+            {
+                if (sayi % i == 0)
+                {
+                    toplam = toplam + i;
+                }
+            }
+
+            return toplam == sayi;
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sayi giriniz");
+            int sayi = int.Parse(Console.ReadLine());
+            if (MukemmelSayıMı(sayi))
+            {
+                Console.WriteLine("girilen sayı mükemmel sayıdır");
+            }
+            else
+            {
+                Console.WriteLine("girilen sayı mükemmel sayı değildir");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
