ba0e632d035848bfb868807027f37b4c47021c41
dabedee <83783289+dabedee@users.noreply.github.com>
2022-12-17 05:50:24 +0300

    ww

--- primeNumber/Program.cs
+++ primeNumber/Program.cs
@@ -10,26 +10,63 @@ namespace primeNumber
     {
         static void Main(string[] args)
         {
-            int sayi, i, sayac = 0;
-            Console.WriteLine("sayı giriniz = ");
-            sayi = int.Parse(Console.ReadLine());
-            for(i =2; i <= sayi; i++)
+            //int sayi, i, sayac = 0;
+            //Console.WriteLine("sayı giriniz = ");
+            //sayi = int.Parse(Console.ReadLine());
+            //for(i =2; i <= sayi; i++)
+            //{
+            //    if (sayi%i==0)
+            //    {
+            //        sayac++;
+            //    }
+            //}
+            //if (sayac == 1)
+            //{
+            //    Console.WriteLine("asal sayıdır");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("asal sayı değildir ");
+            //}
+
+            // 50 200 ARALIĞINDAKŞ ASALLAR 
+
+            int i, j;
+            for(i = 50; i <= 200; i++)
             {
-                if (sayi%i==0)
+                if (i < 2)
                 {
-                    sayac++;
+                    continue;
+                }
+                bool isPrime = true;
+                for (j = 2; j <= i / 2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    Console.WriteLine(i+" ");
                 }
             }
-            if (sayac == 1)
-            {
-                Console.WriteLine("asal sayıdır");
-            }
-            else
-            {
-                Console.WriteLine("asal sayı değildir ");
-            }
+
+
+
+
+
+
+
+
+
 
             Console.ReadLine();
+
+
+
+
         }
     }
 }
