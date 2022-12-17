0826ce2056556056b64b4c67c4ef0bce1dece6f8
dabedee <83783289+dabedee@users.noreply.github.com>
2022-12-17 05:40:19 +0300

    k

---
+++ primeNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi, i, sayac = 0;
+            Console.WriteLine("sayı giriniz = ");
+            sayi = int.Parse(Console.ReadLine());
+            for(i =2; i <= sayi; i++)
+            {
+                if (sayi%i==0)
+                {
+                    sayac++;
+                }
+            }
+            if (sayac == 1)
+            {
+                Console.WriteLine("asal sayıdır");
+            }
+            else
+            {
+                Console.WriteLine("asal sayı değildir ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
