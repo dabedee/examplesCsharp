fb947f37b773ab5e558511e04e6c39960f95c73e
dabedee <83783289+dabedee@users.noreply.github.com>
2022-12-19 05:07:05 +0300

    abc

---
+++ diziOrnekleri/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diziOrnekleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, sayi, i;
+            Console.Write("Sınıf mevcudunu giriniz = ");
+            n = int.Parse(Console.ReadLine());
+            string[] myArr = new string[55];
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("ogrenci adını giriniz = ");
+                myArr[i] = Console.ReadLine();
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ diziOrnekleri2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diziOrnekleri2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pozitif = 0, negatif = 0, notr = 0, i;
+            Random rnd = new Random();
+            int[] myArr = new int[40];
+            for (i = 0; i < 40; i++)
+            {
+                myArr[i] = rnd.Next(-100, 100);
+            }
+            foreach (int sayi in myArr)
+            {
+                Console.WriteLine(sayi);
+                if (sayi > 0)
+                {
+                    pozitif++;
+                }
+                else if (sayi<0)
+                {
+                    negatif++;
+
+                }
+                else
+                {
+                    notr++;
+                }
+
+            }
+            Console.WriteLine("{0} tane pozitif sayı vardır",pozitif);
+            Console.WriteLine("{0} tane negatif sayı vardır ",negatif);
+            Console.WriteLine("{0} tane sıfır vardır ",notr);
+            Console.ReadLine();
+        }
+    }
+}
